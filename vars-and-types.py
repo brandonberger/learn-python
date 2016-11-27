24a188d5634bfc80a92b92bfccab54e6af52bb42
Brandon Berger <bdonberger@gmail.com>
2016-11-27 18:38:43 -0500

    Variables & Types

---
+++ vars-and-types.py
@@ -0,0 +1,48 @@
+## Python is completely object oriented.
+## You do not need to declare variables or their types
+## Every variable in Python is an object
+
+## Python Supports two types of numbers: 
+## Integers & Floating Point Numbers
+
+## Defining an integer i.e
+myint = 7
+
+## Defining a floating point number i.e
+myfloat = 7.0
+## OR 
+myfloat = float(7)
+
+
+## Strings are defined with single or double quotes
+## i.e
+mystring = 'hello'
+mystring = "hello"
+
+
+## Simple operators can be used on numbers & strings
+## Adding numbers i.e
+one = 1
+two = 2
+three = one + two
+
+## Adding strings i.e
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+
+## Assignments can be done on more than one variable at the same time
+## i.e 
+a, b = 3, 4
+
+## Adding numbers & strings will not work
+
+
+## Exercise
+mystring = "hello"
+myfloat  = 10.0
+myint    = 20
+
+print mystring
+print myfloat
+print myint
